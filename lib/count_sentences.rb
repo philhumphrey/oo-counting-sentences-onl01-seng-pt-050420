ae9a68b0f29479dc732fc163a11352680dab3e8a
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-22 00:30:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = []
+    self.split.each do |word|
+      if word.include?(".")
+       sentences << word 
+        
+      elsif 
+      word.include?("?")
+      sentences << word
+      
+      elsif 
+      word.include?("!")
+      sentences << word
+    else
   end
 end
+      sentences.length
+    end
+  end
+
+
